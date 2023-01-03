e9cb5cb726c8fd5af24f35d181bbe2e22e3295af
Your Name <melnikov150990@mail.ru>
2023-01-03 15:18:27 +0500

    navigation

---
+++ app/src/main/java/ru/asmelnikov/android/moviesearchapp/ui/movie/MovieFragment.kt
@@ -0,0 +1,22 @@
+package ru.asmelnikov.android.moviesearchapp.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.asmelnikov.android.moviesearchapp.R
+
+class MovieFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+}
