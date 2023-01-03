1a5910a28d123381bc79abdb97f5e348de500e42
Your Name <melnikov150990@mail.ru>
2023-01-03 17:05:31 +0500

    glide, layouts

---
+++ app/src/main/java/ru/asmelnikov/android/moviesearchapp/utils/BindingAdapters.kt
@@ -0,0 +1,12 @@
+package ru.asmelnikov.android.moviesearchapp.utils
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("load")
+fun loadImage(view: ImageView, url: String) {
+
+    Glide.with(view).load(url).into(view)
+
+}
