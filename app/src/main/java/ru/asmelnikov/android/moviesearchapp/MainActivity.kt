db942716208b9f7f8fd6b6f55374b7b25eed3391
Your Name <melnikov150990@mail.ru>
2023-01-03 15:28:03 +0500

    Hilt

--- app/src/main/java/ru/asmelnikov/android/moviesearchapp/MainActivity.kt
+++ app/src/main/java/ru/asmelnikov/android/moviesearchapp/MainActivity.kt
@@ -2,8 +2,13 @@ package ru.asmelnikov.android.moviesearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/ru/asmelnikov/android/moviesearchapp/hilt/HiltModules.kt
@@ -0,0 +1,14 @@
+package ru.asmelnikov.android.moviesearchapp.hilt
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object HiltModules {
+
+
+
+
+}
